a8c012238a71d1faf7f5b5a00880a061a5b363a3
akdv88 <akdv88@users.noreply.github.com>
2016-12-07 17:43:53 +0300

    Update and rename sedparser.sh to dnspacmacker.sh

--- dnspacmacker.sh
+++ dnspacmacker.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Shell script to make PAC file to proxify Russian blocked sites.
+# It uses only dnsDomainIs function to parse.
+# Works with Crome and Firefox browsers.
 
 dir="/tmp"
 inlist="$dir/curl-list"
