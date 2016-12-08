4e87fa82d31e0283c63c19deb992b3ac9d6d8e4f
akdv88 <akdv88@users.noreply.github.com>
2016-12-08 17:45:26 +0300

    Update lighttpd-entrypoint.sh

--- PAC/Lighttpd/lighttpd-entrypoint.sh
+++ PAC/Lighttpd/lighttpd-entrypoint.sh
@@ -4,4 +4,15 @@ set -e
 
 ln -s /pac /var/www/localhost/
 
+cat << EOF > /var/www/localhost/pac/default.html
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Tor Proxy Pac</h1>
+
+</body>
+</html>
+EOF
+
 exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
